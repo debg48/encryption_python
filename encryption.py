8d79ef1b10e03d554db9444ec6b2972dbb76163c
debg48 <debgandhar4000@gmail.com>
2023-02-07 11:05:02 +0530

    decrption mechanism added

--- encryption.py
+++ encryption.py
@@ -1,24 +1,50 @@
-from itertools import cycle 
+class CyclicArray():
+    array = ["a","b","c",'d',"e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    index = 0
+    def next_item(self):
+        self.index +=1
+        if self.index >= len(self.array):
+            self.index = 0
+        return self.array[self.index]
+    def prev_item(self):
+        self.index-=1
+        if self.index<0:
+            self.index = len(self.array)-1
+        return self.array[self.index]
 
-lst = ["a","b","c",'d',"e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-pool = cycle(lst)
-
-data="d @#$%eb"
-data=data.lower()
-
-enc = ''
-key=2
-
-for i in range(len(data)):
-    temp=next(pool)
-    if data[i] not in lst:
-        enc=enc+ data[i]
-    else:
-        while(temp!=data[i]):
-            temp=(next(pool))
-        for j in range(key):
-            temp= next(pool) 
-        enc= enc + str(temp) 
-
-print(enc)
+value=int(input("Press 1 for  data encryption and 2 for data decryption\n"))
+if value == 1 or value == 2:
+    data=input("Enter your data \n")
+    data=data.lower()
+    lst=CyclicArray()
+    enc = ''
+    key=2
+    if value == 1:
+        for i in range(len(data)):
+            temp=lst.next_item()
+            if data[i] not in lst.array:
+                enc=enc+ data[i]
+            else:
+                while(temp!=data[i]):
+                    temp=(lst.next_item())
+                for j in range(key):
+                    temp= lst.next_item() 
+                enc= enc + str(temp) 
+        print("The encrypted data is")
+        print(enc)
+    if value == 2 :
+        for i in range(len(data)):
+            temp=lst.prev_item()
+            if data[i] not in lst.array:
+                enc=enc+ data[i]
+            else:
+                while(temp!=data[i]):
+                    temp=(lst.prev_item())
+                for j in range(key):
+                    temp= lst.prev_item() 
+                enc= enc + str(temp) 
+        print("The decrypted data is")
+        print(enc)
+        
+else:
+    print("Enter a valid response!")
